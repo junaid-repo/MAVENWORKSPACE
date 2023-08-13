842d795bb9ca93b0d018316b405c1b6740fe7550
junaid-repo <junaidraza3001@gmail.com>
2023-08-13 16:58:25 +0530

    testing

--- user-service/src/main/java/com/mircorservices/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/mircorservices/userservice/UserServiceApplication.java
@@ -1,13 +1,22 @@
 package com.mircorservices.userservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
+	Logger log = LoggerFactory.getLogger(this.getClass());
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserServiceApplication.class, args);
+		//entered into the main method of UserServiceApplication
+
 	}
 
 }
