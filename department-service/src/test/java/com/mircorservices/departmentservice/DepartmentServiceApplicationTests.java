648c46529196b1459770190bf4d4772782abf58c
junaid-repo <junaidraza3001@gmail.com>
2023-08-13 16:49:28 +0530

    microservices first

---
+++ department-service/src/test/java/com/mircorservices/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mircorservices.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
